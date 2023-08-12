8323072f5e13e8fad1ecc03a25653010afc723ae
Paul Joey Clark <joeytwiddle@gmail.com>
2023-08-13 01:43:06 +0800

    feat: clarify definitions, to get more feat than refactor

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -32,14 +32,14 @@ const commitTypes: Record<CommitType, string> = {
 		JSON.stringify({
 			docs: 'Documentation only changes',
 			style: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-			refactor: 'A code change that neither fixes a bug nor adds a feature',
+			refactor: 'A code change that neither fixes a bug, nor adds a feature, nor changes what the code does',
 			perf: 'A code change that improves performance',
 			test: 'Adding missing tests or correcting existing tests',
 			build: 'Changes that affect the build system or external dependencies',
 			ci: 'Changes to our CI configuration files and scripts',
 			chore: "Other changes that don't modify src or test files",
 			revert: 'Reverts a previous commit',
-			feat: 'A new feature',
+			feat: 'A new feature or an improvement to the existing code',
 			fix: 'A bug fix',
 		}, null, 2)
 	}`,
